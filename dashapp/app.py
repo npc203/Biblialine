fb3655cb1c4bc12162218fbcfbe78171f4c9da85
npc203 <samjerub@gmail.com>
2022-12-27 21:58:42 +0530

    gunicorn deploy

--- dashapp/app.py
+++ dashapp/app.py
@@ -3,6 +3,7 @@ import dash_bootstrap_components as dbc
 from dash import Dash, dcc, html
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 # app.layout = html.Div(
 #     [
